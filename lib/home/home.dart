fe2c8ff86c57a6a9a9b1d1197863879eab0dd9a1
Yuto Tsukamoto <t.fr.swim@icloud.com>
2022-04-17 11:34:31 +0900

    Delete MyPage + modify naming

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../record/record.dart';
-import '../mypage/mypage.dart';
 import '../setup/setup.dart';
 import 'home_model.dart';
 
 // Header部分
 class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,47 +17,31 @@ class Home extends StatelessWidget {
           IconButton(
             // Screen transition to loginPage
             onPressed: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => Record()));
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const Record()));
             },
             icon: const Icon(Icons.bar_chart),
           ),
         ],
       ),
       drawerEdgeDragWidth: 0,
-      drawer: Container(
+      drawer: SizedBox(
         width: 200,
         child: SafeArea(
           child: Drawer(
             child: ListView(
               children: [
-                SizedBox(
+                const SizedBox(
                     height: 60,
                     child: DrawerHeader(
                       child: Text('各種ページ'),
                     )),
                 ListTile(
-                  title: Text("記録"),
-                  trailing: Icon(Icons.bar_chart),
+                  title: const Text("設定"),
+                  trailing: const Icon(Icons.settings),
                   onTap: () {
                     Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Record()));
-                  },
-                ),
-                ListTile(
-                  title: Text("設定"),
-                  trailing: Icon(Icons.settings),
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Setup()));
-                  },
-                ),
-                ListTile(
-                  title: Text("マイページ"),
-                  trailing: Icon(Icons.account_circle),
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => MyPage()));
+                        MaterialPageRoute(builder: (context) => const Setup()));
                   },
                 ),
               ],
@@ -76,11 +61,11 @@ class HomeBody extends ConsumerWidget {
   HomeBody({Key? key}) : super(key: key);
 
   // 変数宣言
-  String result_msg = '';
+  String _resultMessage = '';
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final ins = ref.watch(ins_changeProvider);
+    final ins = ref.watch(homeChangeProvider);
 
     return Column(
       children: [
@@ -100,38 +85,38 @@ class HomeBody extends ConsumerWidget {
 
         // element2:SubmitButton
         TextButton(
-          child: Text("登録"),
+          child: const Text("登録"),
           onPressed: () async {
-            result_msg = await ins.add_register();
+            _resultMessage = await ins.add_register();
 
             /// scackbar部分は別関数として切り出したい。ifの乱立は可読性悪い。
             // 登録成功時
-            if (result_msg == register_ok) {
+            if (_resultMessage == successMessage) {
               ScaffoldMessenger.of(context).showSnackBar(SnackBar(
                 backgroundColor: Colors.lightBlueAccent,
-                content: Text(result_msg),
+                content: Text(_resultMessage),
               ));
             }
             // 作業時間未選択時
-            else if (result_msg == register_ng) {
+            else if (_resultMessage == mistakeMessage) {
               ScaffoldMessenger.of(context).showSnackBar(SnackBar(
                 backgroundColor: Colors.red,
-                content: Text(result_msg),
+                content: Text(_resultMessage),
               ));
             }
             // 二重登録時
             /// ダイアログ出して、（上書きしますか？)を表示したい
-            // if (result_msg == register_ng) {
+            // if (_resultMessage == register_ng) {
             //   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
             //     backgroundColor: Colors.red,
-            //     content: Text(result_msg),
+            //     content: Text(_resultMessage),
             //   ));
             // }
             // firebase側のエラー時
             else {
               ScaffoldMessenger.of(context).showSnackBar(SnackBar(
                 backgroundColor: Colors.amberAccent,
-                content: Text(result_msg),
+                content: Text(_resultMessage),
               ));
             }
           },
--- lib/home/home_model.dart
+++ lib/home/home_model.dart
@@ -6,7 +6,7 @@ import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 // 定数宣言
-final List<String> items = [
+const List<String> items = [
   '10',
   '20',
   '30',
@@ -14,56 +14,56 @@ final List<String> items = [
   '50',
 ];
 final String time = DateFormat.yMMMEd('ja').format(DateTime.now());
-final String register_ok = "登録しました。";
-final String register_ng = "作業時間が選択されていません。";
+const String successMessage = "登録しました。";
+const String mistakeMessage = "作業時間が選択されていません。";
 
 // Provider宣言
-final ins_changeProvider =
+final homeChangeProvider =
     ChangeNotifierProvider<HomeModel>((ref) => HomeModel());
 
 class HomeModel extends ChangeNotifier {
   String? selectedValue;
 
   // textfiledの文字列をセット
-  void setStr(String? filed_text) {
-    selectedValue = filed_text;
+  void setStr(String? filedText) {
+    selectedValue = filedText;
     notifyListeners();
   }
 
   Future<String> add_register() async {
-    String snackbar_msg = '';
+    String _snackbarMessage = '';
 
     // 作業時間が選択されていれば、Firebaseへの登録処理を実施
     if (selectedValue != null) {
       // 日付情報の取得
-      final String year_month = DateFormat('yyyyMM').format(DateTime.now());
+      final String yearMonth = DateFormat('yyyyMM').format(DateTime.now());
       final String day = DateFormat.d().format(DateTime.now());
       // UserID取得
-      final String? user_id = FirebaseAuth.instance.currentUser?.uid;
+      final String? userId = FirebaseAuth.instance.currentUser?.uid;
 
       // Firestoreに登録
       final doc = FirebaseFirestore.instance
           .collection('users')
-          .doc(user_id)
-          .collection(year_month)
+          .doc(userId)
+          .collection(yearMonth)
           .doc(day);
 
       try {
         await doc.set({
           'minute': selectedValue,
         });
-        snackbar_msg = register_ok;
+        _snackbarMessage = successMessage;
       } catch (e) {
         print(e.toString());
-        snackbar_msg = e.toString();
+        _snackbarMessage = e.toString();
       }
     }
 
     // 作業時間が未選択なら処理せずエラー文だけ返す
     else {
-      snackbar_msg = register_ng;
+      _snackbarMessage = mistakeMessage;
     }
-    return snackbar_msg;
+    return _snackbarMessage;
   }
 }
 
