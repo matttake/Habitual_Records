edf1ce84cfa7a8bf0e94220bce9d55c765d4ebaf
Yuto Tsukamoto <t.fr.swim@icloud.com>
2022-05-03 10:59:02 +0900

    Widget Layout Adjustments

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -32,9 +32,7 @@ class Home extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: HomeBody(),
-      ),
+      body: const HomeBody(),
     );
   }
 }
@@ -59,7 +57,8 @@ class HomeBody extends ConsumerWidget {
 
           return Column(
             children: [
-              Center(
+              Container(
+                padding: const EdgeInsets.only(top: 10),
                 child: Wrap(
                   direction: Axis.horizontal,
                   children: <Widget>[
@@ -68,68 +67,86 @@ class HomeBody extends ConsumerWidget {
                   ],
                 ),
               ),
-              Center(
-                child: DropDown(_homeChangeProvider, _hintText, _targetItems),
-              ),
-              TextButton(
-                child: const Text("登録"),
-                onPressed: () async {
-                  String _resultMessage = '';
-                  bool _overwriteJudgment = true;
-                  bool _checkResult = false;
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 50),
+                      child: Center(
+                        child: DropDown(
+                            _homeChangeProvider, _hintText, _targetItems),
+                      ),
+                    ),
+                    ElevatedButton(
+                      child: const Text("登録"),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blue,
+                        onPrimary: Colors.white,
+                      ),
+                      onPressed: () async {
+                        String _resultMessage = '';
+                        bool _overwriteJudgment = true;
+                        bool _checkResult = false;
 
-                  // ドロップダウンの値が選択されているなら、Firestoreに本日の値が既に登録済みかを確認。
-                  if (_homeChangeProvider.dropdownSelectedValue != null) {
-                    _checkResult = await _homeChangeProvider.checkRegistered();
-                  } else {
-                    _resultMessage = mistakeMessage;
-                    _overwriteJudgment = false;
-                  }
+                        // ドロップダウンの値が選択されているなら、Firestoreに本日の値が既に登録済みかを確認。
+                        if (_homeChangeProvider.dropdownSelectedValue != null) {
+                          _checkResult =
+                              await _homeChangeProvider.checkRegistered();
+                        } else {
+                          _resultMessage = mistakeMessage;
+                          _overwriteJudgment = false;
+                        }
 
-                  // 既に登録済みならダイアログ表示
-                  if (_checkResult == true) {
-                    _overwriteJudgment = await dialog(context);
-                  }
+                        // 既に登録済みならダイアログ表示
+                        if (_checkResult == true) {
+                          _overwriteJudgment = await dialog(context);
+                        }
 
-                  // REVIEW: なぜかprintが2重呼び出しされてる。要確認。
-                  // print('bool: ' + _overwriteJudgment.toString());
-                  // print('Message: ' + _resultMessage);
+                        // REVIEW: なぜかprintが2重呼び出しされてる。要確認。
+                        // print('bool: ' + _overwriteJudgment.toString());
+                        // print('Message: ' + _resultMessage);
 
-                  // 既存登録アイテム無し or 上書きOKなら、登録処理実行
-                  if (_overwriteJudgment == true) {
-                    _resultMessage = await _homeChangeProvider.addRegister();
-                  }
+                        // 既存登録アイテム無し or 上書きOKなら、登録処理実行
+                        if (_overwriteJudgment == true) {
+                          _resultMessage =
+                              await _homeChangeProvider.addRegister();
+                        }
 
-                  /// scackbar部分は別関数として切り出したい。ifの乱立は可読性悪い。
-                  // 登録成功時
-                  if (_resultMessage == successMessage) {
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      backgroundColor: Colors.lightBlueAccent,
-                      content: Text(_resultMessage),
-                    ));
-                    _homeChangeProvider.iniStr();
-                  }
-                  // 作業時間未選択時
-                  else if (_resultMessage == mistakeMessage) {
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      backgroundColor: Colors.red,
-                      content: Text(_resultMessage),
-                    ));
-                    // 上書きNGにした場合
-                  } else if (_resultMessage == '') {
-                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                      backgroundColor: Colors.lightGreen,
-                      content: Text('登録はキャンセルされました'),
-                    ));
-                    _homeChangeProvider.iniStr();
-                    // FireStore側のエラーの場合
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      backgroundColor: Colors.amberAccent,
-                      content: Text(_resultMessage),
-                    ));
-                  }
-                },
+                        /// scackbar部分は別関数として切り出したい。ifの乱立は可読性悪い。
+                        // 登録成功時
+                        if (_resultMessage == successMessage) {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            backgroundColor: Colors.lightBlueAccent,
+                            content: Text(_resultMessage),
+                          ));
+                          _homeChangeProvider.iniStr();
+                        }
+                        // 作業時間未選択時
+                        else if (_resultMessage == mistakeMessage) {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            backgroundColor: Colors.red,
+                            content: Text(_resultMessage),
+                          ));
+                          // 上書きNGにした場合
+                        } else if (_resultMessage == '') {
+                          ScaffoldMessenger.of(context)
+                              .showSnackBar(const SnackBar(
+                            backgroundColor: Colors.lightGreen,
+                            content: Text('登録はキャンセルされました'),
+                          ));
+                          _homeChangeProvider.iniStr();
+                          // FireStore側のエラーの場合
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            backgroundColor: Colors.amberAccent,
+                            content: Text(_resultMessage),
+                          ));
+                        }
+                      },
+                    ),
+                  ],
+                ),
               ),
             ],
           );
